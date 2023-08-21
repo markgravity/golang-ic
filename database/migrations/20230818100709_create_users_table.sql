ca7074d3e48a303cad3e827e6af97366d4729d4f
MarkG <markgravity.in@gmail.com>
2023-08-21 11:10:06 +0700

    [#32] Mark email as unique

--- database/migrations/20230818100709_create_users_table.sql
+++ database/migrations/20230818100709_create_users_table.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 CREATE TABLE "public"."users" (
     id uuid DEFAULT uuid_generate_v1(),
-    "email" text,
+    "email" text UNIQUE,
     "encrypted_password" text,
     "created_at" timestamptz,
     "updated_at" timestamptz,
