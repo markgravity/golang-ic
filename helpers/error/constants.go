8091e561c235d2b1dbac5c5b68a3b7ed5b947102
MarkG <markgravity.in@gmail.com>
2023-08-23 15:33:38 +0700

    [#25] Remove redundant files

--- helpers/error/constants.go
+++
@@ -1,9 +0,0 @@
-package errorhelpers
-
-const (
-	NotFound = "NOT FOUND"
-
-	NotFoundCode            = "ERROR_NOT_FOUND"
-	TimeoutCode             = "ERROR_TIMEOUT"
-	InternalServerErrorCode = "ERROR_INTERNAL_SERVER_ERROR"
-)
