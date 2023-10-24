1680e3ab481400ea1afc25a58ae55bf417d169d6
MarkG <markgravity.in@gmail.com>
2023-10-24 14:48:31 +0700

    [#9] Remove redundant context

--- lib/api/v1/serializers/keyword_detail_test.go
+++ lib/api/v1/serializers/keyword_detail_test.go
@@ -11,27 +11,25 @@ import (
 
 var _ = Describe("KeywordDetailSerializer", func() {
 	Describe("#Data", func() {
-		Context("given a VALID params", func() {
-			It("returns serialized data", func() {
-				user := fabricators.FabricateTester()
-				keyword := fabricators.FabricateKeyword("test", user)
+		It("returns serialized data", func() {
+			user := fabricators.FabricateTester()
+			keyword := fabricators.FabricateKeyword("test", user)
 
-				serializer := serializers.KeywordDetailSerializer{
-					Keyword: *keyword,
-				}
+			serializer := serializers.KeywordDetailSerializer{
+				Keyword: *keyword,
+			}
 
-				result := serializer.Data()
+			result := serializer.Data()
 
-				Expect(result.ID).To(Equal(keyword.Base.ID.String()))
-				Expect(result.Text).To(Equal(keyword.Text))
-				Expect(result.Status).To(Equal(string(keyword.Status)))
-				Expect(result.LinksCount).To(Equal(keyword.LinksCount))
-				Expect(result.NonAdwordLinks).To(Equal(keyword.NonAdwordLinks))
-				Expect(result.NonAdwordLinksCount).To(Equal(keyword.NonAdwordLinksCount))
-				Expect(result.AdwordLinks).To(Equal(keyword.AdwordLinks))
-				Expect(result.AdwordLinksCount).To(Equal(keyword.AdwordLinksCount))
-				Expect(result.HtmlCode).To(Equal(keyword.HtmlCode))
-			})
+			Expect(result.ID).To(Equal(keyword.Base.ID.String()))
+			Expect(result.Text).To(Equal(keyword.Text))
+			Expect(result.Status).To(Equal(string(keyword.Status)))
+			Expect(result.LinksCount).To(Equal(keyword.LinksCount))
+			Expect(result.NonAdwordLinks).To(Equal(keyword.NonAdwordLinks))
+			Expect(result.NonAdwordLinksCount).To(Equal(keyword.NonAdwordLinksCount))
+			Expect(result.AdwordLinks).To(Equal(keyword.AdwordLinks))
+			Expect(result.AdwordLinksCount).To(Equal(keyword.AdwordLinksCount))
+			Expect(result.HtmlCode).To(Equal(keyword.HtmlCode))
 		})
 	})
 })
